2c86f356f89c2728a9e2ad723cebb37a0ff3d630
H2oPtic <96192593+H2oPtic@users.noreply.github.com>
2022-01-19 17:23:25 -0500

    Update Matplotlib-ErrorBars.py

--- Matplotlib-ErrorBars.py
+++ Matplotlib-ErrorBars.py
@@ -5,7 +5,7 @@ ounces_of_milk = [6, 9, 4, 0, 9, 0]
 error = [0.6, 0.9, 0.4, 0, 0.9, 0]
 
 
-plt.bar(range(len(ounces_of_milk)), ounces_of_milk, yerr=yerr, capsize=5)
-yerr = error
+plt.bar(range(len(ounces_of_milk)), ounces_of_milk, yerr=error, capsize=5)
+
 
 plt.show()
