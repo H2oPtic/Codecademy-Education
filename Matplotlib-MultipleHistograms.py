774a2088357f27af896fc7df7856bb776f9eae98
H2oPtic <96192593+H2oPtic@users.noreply.github.com>
2022-01-19 17:04:42 -0500

    Create Matplotlib-MultipleHistograms.py

---
+++ Matplotlib-MultipleHistograms.py
@@ -0,0 +1,8 @@
+rom matplotlib import pyplot as plt
+from script import sales_times1
+from script import sales_times2
+
+plt.hist(sales_times1, bins=20, alpha=0.4, normed=True)
+plt.hist(sales_times2, bins=20, alpha=0.4, normed=True)
+
+plt.show()
